840b06e685e8e5311adb5fc841063299998c7405
AlexanderFromEarth <alexanderfromearth@gmail.com>
2019-04-02 16:05:28 +0500

    Interface

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Win32;
+using MVVM;
+
+namespace People.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        Models.ScriptRunner _model;
+        string _videoInfo;
+        string _loadPath;
+        string _savePath;
+
+        public MainViewModel() => this._model = new Models.ScriptRunner();
+
+        public string VideoInfo
+        {
+            get => this._videoInfo;
+            set
+            {
+                this._videoInfo = value;
+                this.OnPropertyChanged(nameof(this.VideoInfo));
+            }
+        }
+
+        public string LoadPath
+        {
+            get => this._loadPath;
+            set
+            {
+                this._loadPath = value;
+                this.OnPropertyChanged(nameof(this.LoadPath));
+            }
+        }
+
+        public ICommand Load => new Command((obj) =>
+        {
+            var fd = new OpenFileDialog();
+            this.LoadPath = "Suka";
+            if (fd.ShowDialog() == true) this.LoadPath = fd.FileName;
+        });
+    }
+}
